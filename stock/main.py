a948be44cf2043c60def1372caacaf42f6d20077
nonoivan1234 <nonoivan0627@gmail.com>
2021-02-27 01:49:25 +0800

    init_

---
+++ stock/main.py
@@ -0,0 +1,19 @@
+import requests
+import numpy as np
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+
+url = "https://query1.finance.yahoo.com/v8/finance/chart/2330.TW"
+r = requests.get(url)
+
+data = json.loads(r.text)
+df = pd.DataFrame(
+    data["chart"]["result"][0]["indicators"]["quote"][0],
+    index=pd.to_datetime(
+        np.array(data["chart"]["result"][0]["timestamp"]) * 1000 * 1000 * 1000
+    ),
+)
+
+print(df.head())
+plt.plot(df.close)
